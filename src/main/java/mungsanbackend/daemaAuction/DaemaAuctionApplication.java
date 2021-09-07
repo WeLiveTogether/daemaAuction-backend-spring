a943390e755f91725a2c215dec06aae27b5d82f9
SonYoonSeok <dbstjr5517@naver.com>
2021-09-07 17:03:24 +0900

    초기설정

---
+++ src/main/java/mungsanbackend/daemaAuction/DaemaAuctionApplication.java
@@ -0,0 +1,13 @@
+package mungsanbackend.daemaAuction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaemaAuctionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaemaAuctionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mungsanbackend/daemaAuction/DaemaAuctionApplicationTests.java
@@ -0,0 +1,13 @@
+package mungsanbackend.daemaAuction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaemaAuctionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
