46cf581e23cfa44cfb493809c6dcd77fd198638e
SonYoonSeok <dbstjr5517@naver.com>
2021-09-12 22:33:05 +0900

    ⚡️ :: Repository 추가

---
+++ src/main/java/mungsanbackend/daemaAuction/domain/category/subCategory/SubCategoryRepository.java
@@ -0,0 +1,6 @@
+package mungsanbackend.daemaAuction.domain.category.subCategory;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SubCategoryRepository extends JpaRepository<SubCategory, Long> {
+}
---
+++ src/main/java/mungsanbackend/daemaAuction/domain/product/productImage/ProductImageRepository.java
@@ -0,0 +1,6 @@
+package mungsanbackend.daemaAuction.domain.product.productImage;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductImageRepository extends JpaRepository<ProductImage, Long> {
+}
