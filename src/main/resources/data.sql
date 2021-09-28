3e358aaf77ab544794af0ca083b2694563ce51ab
SonYoonSeok <dbstjr5517@naver.com>
2021-09-28 18:56:29 +0900

    ⚡️ :: subCategory 값 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -15,3 +15,24 @@ VALUES("생활용품");
 
 INSERT INTO category(name)
 VALUES("완구/취미");
+
+INSERT INTO sub_Category(name, category_id)
+VALUES("상의", 1);
+
+INSERT INTO sub_Category(name, category_id)
+VALUES("하의", 1);
+
+INSERT INTO sub_Category(name, category_id)
+VALUES("액세서리", 1);
+
+INSERT INTO sub_Category(name, category_id)
+VALUES("신발", 1);
+
+INSERT INTO sub_Category(name, category_id)
+VALUES("성별", 1);
+
+INSERT INTO sub_Category(name, category_id)
+VALUES("전공도서", 4);
+
+INSERT INTO sub_Category(name, category_id)
+VALUES("교양도서", 4);
