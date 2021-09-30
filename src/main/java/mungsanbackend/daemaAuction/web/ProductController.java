550c57cf02f4169cc42f68860c0af19b14cddd69
SonYoonSeok <dbstjr5517@naver.com>
2021-09-30 21:50:07 +0900

    ⚡️ :: /api/products 기능
    Product 리스트를 받아옴

--- src/main/java/mungsanbackend/daemaAuction/web/ProductController.java
+++ src/main/java/mungsanbackend/daemaAuction/web/ProductController.java
@@ -1,2 +1,24 @@
-package mungsanbackend.daemaAuction.web;public class ProductController {
+package mungsanbackend.daemaAuction.web;
+
+import lombok.RequiredArgsConstructor;
+import mungsanbackend.daemaAuction.service.ProductService;
+import mungsanbackend.daemaAuction.web.dto.response.ProductResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api")
+public class ProductController {
+
+    private final ProductService productService;
+
+    @GetMapping("/products")
+    public ResponseEntity<List<ProductResponse>> productList() {
+        return ResponseEntity.ok(productService.getProductList());
+    }
 }
