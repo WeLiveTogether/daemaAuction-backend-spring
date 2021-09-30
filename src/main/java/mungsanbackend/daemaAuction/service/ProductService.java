631ca6d6c7ef37a2b28c290ea7d7b764d7a58028
SonYoonSeok <dbstjr5517@naver.com>
2021-09-30 17:18:50 +0900

    ⚡️ :: 모든 Product return

--- src/main/java/mungsanbackend/daemaAuction/service/ProductService.java
+++ src/main/java/mungsanbackend/daemaAuction/service/ProductService.java
@@ -1,2 +1,24 @@
-package mungsanbackend.daemaAuction.service;public class ProductService {
+package mungsanbackend.daemaAuction.service;
+
+import lombok.RequiredArgsConstructor;
+import mungsanbackend.daemaAuction.repository.CategoryRepository;
+import mungsanbackend.daemaAuction.repository.ProductRepository;
+import mungsanbackend.daemaAuction.repository.SubCategoryRepository;
+import mungsanbackend.daemaAuction.web.dto.response.ProductResponse;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class ProductService {
+
+    private final CategoryRepository categoryRepository;
+    private final SubCategoryRepository subCategoryRepository;
+    private final ProductRepository productRepository;
+
+    public List<ProductResponse> getProductList() {
+        return productRepository.findAll().stream().map(ProductResponse::of).collect(Collectors.toList());
+    }
 }
