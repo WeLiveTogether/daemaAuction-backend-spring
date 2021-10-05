1b7f5528ba0b920e6158d8eaf551ea1cd7ffed50
SonYoonSeok <dbstjr5517@naver.com>
2021-10-05 11:34:26 +0900

    ⚡️ :: /api/products Swagger 추가

--- src/main/java/mungsanbackend/daemaAuction/web/ProductController.java
+++ src/main/java/mungsanbackend/daemaAuction/web/ProductController.java
@@ -1,5 +1,8 @@
 package mungsanbackend.daemaAuction.web;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import mungsanbackend.daemaAuction.service.ProductService;
 import mungsanbackend.daemaAuction.web.dto.response.ProductResponse;
@@ -17,6 +20,13 @@ public class ProductController {
 
     private final ProductService productService;
 
+    @Operation(summary = "get product list")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "OK !!"),
+            @ApiResponse(responseCode = "400", description = "BAD REQUEST !!"),
+            @ApiResponse(responseCode = "404", description = "NOT FOUND !!"),
+            @ApiResponse(responseCode = "500", description = "INTERNAL SERVER ERROR !!")
+    })
     @GetMapping("/products")
     public ResponseEntity<List<ProductResponse>> productList() {
         return ResponseEntity.ok(productService.getProductList());
