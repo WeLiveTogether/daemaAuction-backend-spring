18698d43d3387cfdc1fa2872a9140d8d7cbdfa42
wkddnjsgh <luca4759@gmail.com>
2021-10-06 11:12:28 +0900

    ⚡️ :: User Entity 수정

--- src/main/java/mungsanbackend/daemaAuction/domain/User.java
+++ src/main/java/mungsanbackend/daemaAuction/domain/User.java
@@ -1,33 +1,69 @@
 package mungsanbackend.daemaAuction.domain;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import mungsanbackend.daemaAuction.domain.BaseTimeEntity;
-import mungsanbackend.daemaAuction.domain.Product;
-import mungsanbackend.daemaAuction.domain.JoinRoom;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.*;
+import mungsanbackend.daemaAuction.oauth.entity.ProviderType;
+import mungsanbackend.daemaAuction.oauth.entity.RoleType;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.util.ArrayList;
 import java.util.List;
 
-@AllArgsConstructor
-@Entity
 @Getter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
 public class User extends BaseTimeEntity {
 
+    @JsonIgnore
     @Id
+    @Column(name = "USER_SEQ")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Long userSeq;
 
-    private String name;
+    @Column(name = "USER_ID", length = 64, unique = true)
+    @NotNull
+    @Size(max = 64)
+    private String userId;
 
-    @Column(length = 4)
-    private int gcn;    // 학번
+    @Column(name = "USERNAME", length = 100)
+    @NotNull
+    @Size(max = 100)
+    private String username;
 
+    @JsonIgnore
+    @Column(name = "PASSWORD", length = 128)
+    @NotNull
+    @Size(max = 128)
+    private String password;
+
+    @Column(name = "EMAIL", length = 512, unique = true)
+    @NotNull
+    @Size(max = 512)
     private String email;
+
+    @Column(name = "EMAIL_VERIFIED_YN", length = 1)
+    @NotNull
+    @Size(min = 1, max = 1)
+    private String emailVerifiedYn;
+
+    @Column(name = "PROFILE_IMAGE_URL", length = 512)
+    @NotNull
+    @Size(max = 512)
+    private String profileImageUrl;
+
+    @Column(name = "PROVIDER_TYPE", length = 20)
+    @Enumerated(EnumType.STRING)
+    @NotNull
+    private ProviderType providerType;
+
+    @Column(name = "ROLE_TYPE", length = 20)
+    @Enumerated(EnumType.STRING)
+    @NotNull
+    private RoleType roleType;
 
     @Builder.Default
     @OneToMany(mappedBy = "user")
@@ -36,4 +72,23 @@ public class User extends BaseTimeEntity {
     @Builder.Default
     @OneToMany(mappedBy = "user")
     private List<JoinRoom> joinRooms= new ArrayList<>();
+
+    public User(
+            @NotNull @Size(max = 64) String userId,
+            @NotNull @Size(max = 100) String username,
+            @NotNull @Size(max = 512) String email,
+            @NotNull @Size(max = 1) String emailVerifiedYn,
+            @NotNull @Size(max = 512) String profileImageUrl,
+            @NotNull ProviderType providerType,
+            @NotNull RoleType roleType
+    ) {
+        this.userId = userId;
+        this.username = username;
+        this.password = "NO_PASS";
+        this.email = email != null ? email : "NO_EMAIL";
+        this.emailVerifiedYn = emailVerifiedYn;
+        this.profileImageUrl = profileImageUrl != null ? profileImageUrl : "";
+        this.providerType = providerType;
+        this.roleType = roleType;
+    }
 }
