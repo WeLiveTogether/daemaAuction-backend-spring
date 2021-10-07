fb74f59c91e8aa3174883797e944869d99fb89c3
SonYoonSeok <dbstjr5517@naver.com>
2021-10-07 21:19:52 +0900

    :bug: :: Security Swagger 설정
    Swagger 리소스 허용

--- src/main/java/mungsanbackend/daemaAuction/config/security/SecurityConfig.java
+++ src/main/java/mungsanbackend/daemaAuction/config/security/SecurityConfig.java
@@ -20,6 +20,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.BeanIds;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -67,7 +68,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .authorizeRequests()
                 .requestMatchers(CorsUtils::isPreFlightRequest).permitAll()
-                .antMatchers("/api/login", "/api/products-latest", "/api/products-popularity").permitAll()
+                .antMatchers("/api/login", "/api/products-latest", "/api/products-popularity", "/v2/api-docs", "/configuration/**", "/swagger*/**", "/webjars/**").permitAll()
                 .antMatchers("/api/**").hasAnyAuthority(RoleType.USER.getCode())
                 .antMatchers("/api/**/admin/**").hasAnyAuthority(RoleType.ADMIN.getCode())
                 .anyRequest().authenticated()
