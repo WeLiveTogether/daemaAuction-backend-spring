e1178c9478b3e8552ef917adf5f14a4014629e3a
wkddnjsgh <luca4759@gmail.com>
2021-10-07 09:13:44 +0900

    :bookmark_tabs: :: CorsProperties 작성

--- src/main/java/mungsanbackend/daemaAuction/DaemaAuctionApplication.java
+++ src/main/java/mungsanbackend/daemaAuction/DaemaAuctionApplication.java
@@ -1,6 +1,7 @@
 package mungsanbackend.daemaAuction;
 
 import mungsanbackend.daemaAuction.config.properties.AppProperties;
+import mungsanbackend.daemaAuction.config.properties.CorsProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -9,7 +10,8 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @EnableJpaAuditing
 @SpringBootApplication
 @EnableConfigurationProperties({
-		AppProperties.class
+		AppProperties.class,
+		CorsProperties.class
 })
 public class DaemaAuctionApplication {
 
