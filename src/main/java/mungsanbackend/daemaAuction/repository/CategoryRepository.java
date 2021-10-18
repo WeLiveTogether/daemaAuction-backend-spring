75424ebb9959ae518eb4ad06962949103bfe534a
SonYoonSeok <dbstjr5517@naver.com>
2021-10-18 23:13:03 +0900

    ⚡️ :: UserId, (Category, SubCategory)로 찾기

--- src/main/java/mungsanbackend/daemaAuction/repository/CategoryRepository.java
+++ src/main/java/mungsanbackend/daemaAuction/repository/CategoryRepository.java
@@ -3,5 +3,8 @@ package mungsanbackend.daemaAuction.repository;
 import mungsanbackend.daemaAuction.domain.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+    Optional<Category> findByName(String name);
 }
