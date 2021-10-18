4d409aba9e80a1dd0cefc40722be1790da837612
SonYoonSeok <dbstjr5517@naver.com>
2021-10-18 23:13:41 +0900

    :bookmark_tabs: :: ProductRequest

--- src/main/java/mungsanbackend/daemaAuction/web/dto/request/ProductRequest.java
+++ src/main/java/mungsanbackend/daemaAuction/web/dto/request/ProductRequest.java
@@ -1,2 +1,13 @@
-package mungsanbackend.daemaAuction.web.dto.request;public class ProductRequest {
+package mungsanbackend.daemaAuction.web.dto.request;
+
+import lombok.Getter;
+
+@Getter
+public class ProductRequest {
+    private String title;
+    private String content;
+    private int immePrice;
+    private int auctionPrice;
+    private String category;
+    private String subCategory;
 }
