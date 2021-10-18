fa21f24034cc0f02cafebdba6e991b130aabc49e
SonYoonSeok <dbstjr5517@naver.com>
2021-10-18 23:17:21 +0900

    :bookmark_tabs: :: @UserId 어노테이션 추가

--- src/main/java/mungsanbackend/daemaAuction/oauth/annotation/UserId.java
+++ src/main/java/mungsanbackend/daemaAuction/oauth/annotation/UserId.java
@@ -1,2 +1,11 @@
-package mungsanbackend.daemaAuction.oauth.annotation;public @interface UserId {
+package mungsanbackend.daemaAuction.oauth.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.PARAMETER)
+public @interface UserId {
 }
