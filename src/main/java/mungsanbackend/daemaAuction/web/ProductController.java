c4b9982aa33c36f5a242eb749f4942120627e1c9
SonYoonSeok <dbstjr5517@naver.com>
2021-10-18 23:19:23 +0900

    ⚡️ :: 경매 글 작성 api

--- src/main/java/mungsanbackend/daemaAuction/web/ProductController.java
+++ src/main/java/mungsanbackend/daemaAuction/web/ProductController.java
@@ -2,13 +2,16 @@ package mungsanbackend.daemaAuction.web;
 
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
+import mungsanbackend.daemaAuction.oauth.annotation.UserId;
 import mungsanbackend.daemaAuction.service.ProductService;
+import mungsanbackend.daemaAuction.service.S3UploaderService;
+import mungsanbackend.daemaAuction.web.dto.request.ProductRequest;
 import mungsanbackend.daemaAuction.web.dto.response.ProductResponse;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -17,6 +20,7 @@ import java.util.List;
 public class ProductController {
 
     private final ProductService productService;
+    private final S3UploaderService s3UploaderService;
 
     @Operation(summary = "Product 최신순으로 정렬")
     @GetMapping("/products-latest")
@@ -29,4 +33,12 @@ public class ProductController {
     public ResponseEntity<List<ProductResponse>> productListByPopularity() {
         return ResponseEntity.ok(productService.getProductListByViews());
     }
+
+    @Operation(summary = "경매 글 작성")
+    @PostMapping("/product")
+    public ResponseEntity<ProductResponse> addProduct(@RequestBody ProductRequest productRequest, @RequestParam("images")MultipartFile multipartFile, @UserId Long userId) throws IOException {
+        s3UploaderService.upload(multipartFile, "static");
+        return ResponseEntity.ok(productService.createProduct(userId, productRequest));
+    }
+
 }
