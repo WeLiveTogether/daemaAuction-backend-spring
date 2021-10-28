dc5d5e3a69c82a226cce24c083073f63b3228e44
wkddnjsgh <luca4759@gmail.com>
2021-10-28 21:00:54 +0900

    ⚡ :: 쓸모없는 import 삭제

--- src/main/java/mungsanbackend/daemaAuction/domain/Product.java
+++ src/main/java/mungsanbackend/daemaAuction/domain/Product.java
@@ -5,7 +5,6 @@ import lombok.*;
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 @AllArgsConstructor
