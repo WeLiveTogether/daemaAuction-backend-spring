dfb732fd0e202dd14423e2db25cfde1c4acd60ec
SonYoonSeok <dbstjr5517@naver.com>
2021-11-04 22:07:41 +0900

    ⚡️ :: ProductImage 생성자

--- src/main/java/mungsanbackend/daemaAuction/domain/ProductImage.java
+++ src/main/java/mungsanbackend/daemaAuction/domain/ProductImage.java
@@ -1,12 +1,16 @@
 package mungsanbackend.daemaAuction.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
+import java.util.Optional;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Entity
 @Getter
 public class ProductImage {
@@ -18,7 +22,14 @@ public class ProductImage {
     @NotNull
     private String url;
 
+    @JsonIgnore
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "product_id")
     private Product product;
+
+    public ProductImage(@NotNull String url, Optional<Product> product) {
+        Product prod = product.get();
+        this.url = url;
+        this.product = prod;
+    }
 }
