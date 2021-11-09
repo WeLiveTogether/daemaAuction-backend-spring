7e791992d9656940ea307b20e057c1f03b1a032c
SonYoonSeok <dbstjr5517@naver.com>
2021-11-09 19:41:33 +0900

    ⚡️ :: Swagger - ProductRequest 설명 추가

--- src/main/java/mungsanbackend/daemaAuction/web/dto/request/ProductRequest.java
+++ src/main/java/mungsanbackend/daemaAuction/web/dto/request/ProductRequest.java
@@ -1,14 +1,28 @@
 package mungsanbackend.daemaAuction.web.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 
 @Getter
 public class ProductRequest {
+    @Schema(description = "유저아이디", example = "1")
     private Long userSeq;
+
+    @Schema(description = "물품명", example = "TV")
     private String title;
+
+    @Schema(description = "물품설명", example = "지리는 TV입니다.")
     private String content;
+
+    @Schema(description = "즉시구매가", example = "15000")
     private int immePrice;
+
+    @Schema(description = "경매시작가", example = "1000")
     private int auctionPrice;
+
+    @Schema(description = "메인카테고리 이름", example = "도서")
     private String category;
+
+    @Schema(description = "서브카테고리 이름", example = "전공도서")
     private String subCategory;
 }
