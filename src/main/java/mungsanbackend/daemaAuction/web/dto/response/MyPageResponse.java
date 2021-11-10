795e9a733f5ca2edff22c5f57c5fb008534402c2
wkddnjsgh <luca4759@gmail.com>
2021-11-10 14:40:42 +0900

    ⚡ :: 마이페이지 DTO 진짜 마지막 수정

--- src/main/java/mungsanbackend/daemaAuction/web/dto/response/MyPageResponse.java
+++ src/main/java/mungsanbackend/daemaAuction/web/dto/response/MyPageResponse.java
@@ -3,10 +3,7 @@ package mungsanbackend.daemaAuction.web.dto.response;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-import mungsanbackend.daemaAuction.domain.Category;
-import mungsanbackend.daemaAuction.domain.ProductSaleStatus;
-import mungsanbackend.daemaAuction.domain.SubCategory;
-import mungsanbackend.daemaAuction.oauth.entity.ProviderType;
+import mungsanbackend.daemaAuction.domain.*;
 
 @Getter
 @Builder
@@ -17,13 +14,26 @@ public class MyPageResponse {
     private String userName;
     private String email;
     private String profileImageUrl;
-    private ProviderType providerType;
 
     // product 자신이 등록한 상품, 거래완료된 상품
     private String title;
     private int auctionPrice;
     private Long views;
-    private ProductSaleStatus SaleStatus;
+    private ProductSaleStatus saleStatus;
     private Category category;
     private SubCategory subCategory;
+
+    public static MyPageResponse of(Product product) {
+        return MyPageResponse.builder()
+                .userName(product.getUser().getUsername())
+                .email(product.getUser().getEmail())
+                .profileImageUrl(product.getUser().getProfileImageUrl())
+                .title(product.getTitle())
+                .auctionPrice(product.getAuctionPrice())
+                .views(product.getViews())
+                .saleStatus(product.getSaleStatus())
+                .category(product.getCategory())
+                .subCategory(product.getSubCategory())
+                .build();
+    }
 }
