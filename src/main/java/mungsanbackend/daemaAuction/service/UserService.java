942cfdfd306bcd4b210a97b16f593e857bff0323
wkddnjsgh <luca4759@gmail.com>
2021-11-10 18:58:44 +0900

    ⚡ :: 유저 정보를 가져오는 메소드 추가

--- src/main/java/mungsanbackend/daemaAuction/service/UserService.java
+++ src/main/java/mungsanbackend/daemaAuction/service/UserService.java
@@ -3,6 +3,7 @@ package mungsanbackend.daemaAuction.service;
 import lombok.RequiredArgsConstructor;
 import mungsanbackend.daemaAuction.domain.User;
 import mungsanbackend.daemaAuction.repository.UserRepository;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -16,5 +17,10 @@ public class UserService {
 
     public User findUserBySeq(Long seq) {
         return userRepository.findUserByUserSeq(seq).orElseThrow(() -> new RuntimeException("User를 찾을 수 없습니다."));
+    }
+
+    public User getUserInfo() {
+        org.springframework.security.core.userdetails.User principal = (org.springframework.security.core.userdetails.User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        return getUser(principal.getUsername());
     }
 }
