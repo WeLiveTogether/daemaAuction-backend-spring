ec020778329c30656befa647fb5264d5a5090b84
wkddnjsgh <luca4759@gmail.com>
2021-11-11 08:35:46 +0900

    ⚡ :: 마이페이지 서비스 추가

--- src/main/java/mungsanbackend/daemaAuction/service/UserService.java
+++ src/main/java/mungsanbackend/daemaAuction/service/UserService.java
@@ -1,15 +1,24 @@
 package mungsanbackend.daemaAuction.service;
 
 import lombok.RequiredArgsConstructor;
+import mungsanbackend.daemaAuction.domain.Product;
 import mungsanbackend.daemaAuction.domain.User;
+import mungsanbackend.daemaAuction.repository.ProductRepository;
 import mungsanbackend.daemaAuction.repository.UserRepository;
+import mungsanbackend.daemaAuction.web.dto.response.MyPageResponse;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
+
     private final UserRepository userRepository;
+    private final ProductRepository productRepository;
 
     public User getUser(String userId) {
         return userRepository.findByUserId(userId);
@@ -22,5 +31,11 @@ public class UserService {
     public User getUserInfo() {
         org.springframework.security.core.userdetails.User principal = (org.springframework.security.core.userdetails.User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         return getUser(principal.getUsername());
+    }
+
+    public List<MyPageResponse> myPage() {
+        User user = getUserInfo();
+        Optional<Product> product = productRepository.findProductByUser(user);
+        return product.stream().map(MyPageResponse::of).collect(Collectors.toList());
     }
 }
