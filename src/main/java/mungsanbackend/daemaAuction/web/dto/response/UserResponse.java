9dcd76cbf29622ce5fba51db0dc0df5e7dcde8ad
wkddnjsgh <luca4759@gmail.com>
2021-11-15 09:18:59 +0900

    ⚡ :: 유저 DTO 추가

---
+++ src/main/java/mungsanbackend/daemaAuction/web/dto/response/UserResponse.java
@@ -0,0 +1,25 @@
+package mungsanbackend.daemaAuction.web.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import mungsanbackend.daemaAuction.domain.User;
+import mungsanbackend.daemaAuction.oauth.entity.ProviderType;
+import mungsanbackend.daemaAuction.oauth.entity.RoleType;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class UserResponse {
+
+    private String userId;
+    private String username;
+    private String email;
+    private String profileImageUrl;
+    private ProviderType providerType;
+    private RoleType roleType;
+
+    public static UserResponse of(User user) {
+        return new UserResponse(user.getUserId(), user.getUsername(), user.getEmail(), user.getProfileImageUrl(), user.getProviderType(), user.getRoleType());
+    }
+}
