cd2919f940e476e485ecfb145581184ab53f5c22
SonYoonSeok <dbstjr5517@naver.com>
2021-11-15 23:17:21 +0900

    :bookmark_tabs: :: SubCategoryNotFoundException 생성

--- src/main/java/mungsanbackend/daemaAuction/exception/SubCategoryNotFoundException.java
+++ src/main/java/mungsanbackend/daemaAuction/exception/SubCategoryNotFoundException.java
@@ -1,2 +1,7 @@
-package mungsanbackend.daemaAuction.exception;public class SubCategoryNotFoundException {
+package mungsanbackend.daemaAuction.exception;
+
+public class SubCategoryNotFoundException extends ElementNotFoundException{
+    public SubCategoryNotFoundException() {
+        super("SubCategory를 찾을 수 없습니다.");
+    }
 }
