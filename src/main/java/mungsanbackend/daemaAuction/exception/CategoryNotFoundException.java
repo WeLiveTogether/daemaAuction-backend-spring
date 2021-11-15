d444ad7342908b2450910e7382f52bdeb35aecef
SonYoonSeok <dbstjr5517@naver.com>
2021-11-15 23:16:49 +0900

    :bookmark_tabs: :: CategoryNotFoundException 생성

--- src/main/java/mungsanbackend/daemaAuction/exception/CategoryNotFoundException.java
+++ src/main/java/mungsanbackend/daemaAuction/exception/CategoryNotFoundException.java
@@ -1,2 +1,7 @@
-package mungsanbackend.daemaAuction.exception;public class CategoryNotFoundException {
+package mungsanbackend.daemaAuction.exception;
+
+public class CategoryNotFoundException extends ElementNotFoundException{
+    public CategoryNotFoundException() {
+        super("Category를 찾을 수 없습니다.");
+    }
 }
