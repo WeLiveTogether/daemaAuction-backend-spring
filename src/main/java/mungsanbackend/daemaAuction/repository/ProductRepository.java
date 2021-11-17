1fbe62dad30595368d6ee9bbd5ce48aab9c25411
wkddnjsgh <luca4759@gmail.com>
2021-11-17 23:33:30 +0900

    ⚡ :: 유저 정보로 상품 찾기 수정

--- src/main/java/mungsanbackend/daemaAuction/repository/ProductRepository.java
+++ src/main/java/mungsanbackend/daemaAuction/repository/ProductRepository.java
@@ -4,8 +4,8 @@ import mungsanbackend.daemaAuction.domain.Product;
 import mungsanbackend.daemaAuction.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
+import java.util.List;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
-    Optional<Product> findProductByUser(User user);
+    List<Product> findProductByUser(User user);
 }
