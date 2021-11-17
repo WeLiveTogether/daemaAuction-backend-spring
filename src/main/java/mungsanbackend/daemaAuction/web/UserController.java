9decfe648a4e507aca56c368014309d72aa678f2
wkddnjsgh <luca4759@gmail.com>
2021-11-17 23:34:03 +0900

    ⚡ :: 마이페이지 API 수정

--- src/main/java/mungsanbackend/daemaAuction/web/UserController.java
+++ src/main/java/mungsanbackend/daemaAuction/web/UserController.java
@@ -6,12 +6,11 @@ import mungsanbackend.daemaAuction.domain.User;
 import mungsanbackend.daemaAuction.service.UserService;
 import mungsanbackend.daemaAuction.web.dto.response.ApiResponse;
 import mungsanbackend.daemaAuction.web.dto.response.UserResponse;
+import mungsanbackend.daemaAuction.web.dto.response.mypage.MyPageResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/api")
@@ -31,7 +30,7 @@ public class UserController {
 
     @Operation(summary = "마이페이지")
     @GetMapping("/mypage")
-    public ResponseEntity<List<MyPageResponse>> myPage() {
+    public ResponseEntity<MyPageResponse> myPage() {
         return ResponseEntity.ok(userService.myPage());
     }
 }
