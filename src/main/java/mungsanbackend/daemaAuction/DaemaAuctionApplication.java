feef7122fe9e3f6821b04edc73f92c37d016d875
SonYoonSeok <dbstjr5517@naver.com>
2021-11-20 14:47:35 +0900

    :bug: :: WebConfig 생성
    CORS 해결

--- src/main/java/mungsanbackend/daemaAuction/DaemaAuctionApplication.java
+++ src/main/java/mungsanbackend/daemaAuction/DaemaAuctionApplication.java
@@ -6,11 +6,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.web.bind.annotation.CrossOrigin;
 
 @EnableJpaAuditing
 @SpringBootApplication
-@CrossOrigin(origins = {"https://daemaauction.herokuapp.com", "http://localhost:3000", "http://localhost:8080"})
 @EnableConfigurationProperties({
 		AppProperties.class,
 		CorsProperties.class
