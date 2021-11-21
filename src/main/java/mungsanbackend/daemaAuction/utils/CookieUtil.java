a539bb1d5d5fb31869abb55ec69adfd704004554
wkddnjsgh <luca4759@gmail.com>
2021-11-21 15:56:14 +0900

    ⚡ :: Cookie path 설정

--- src/main/java/mungsanbackend/daemaAuction/utils/CookieUtil.java
+++ src/main/java/mungsanbackend/daemaAuction/utils/CookieUtil.java
@@ -16,6 +16,7 @@ public class CookieUtil {
         if (cookies != null && cookies.length > 0) {
             for (Cookie cookie : cookies) {
                 if (name.equals(cookie.getName())) {
+                    cookie.setPath("/");
                     return Optional.of(cookie);
                 }
             }
