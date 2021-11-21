46e04b0977943b205d9c7d2f2eb791a6ebd37adf
wkddnjsgh <luca4759@gmail.com>
2021-11-21 23:15:40 +0900

    ⚡ :: 물품 판매 상태 업데이트 쿼리 추가

--- src/main/java/mungsanbackend/daemaAuction/repository/ProductRepository.java
+++ src/main/java/mungsanbackend/daemaAuction/repository/ProductRepository.java
@@ -3,9 +3,19 @@ package mungsanbackend.daemaAuction.repository;
 import mungsanbackend.daemaAuction.domain.Product;
 import mungsanbackend.daemaAuction.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
     List<Product> findProductByUser(User user);
+    Product findProductById(Long productId);
+
+
+    @Modifying
+    @Query(value = "UPDATE Product p SET p.sale_status= 'SOLD_OUT' WHERE p.id = :id", nativeQuery = true)
+    void updateStatus(@Param("id") Long id) throws Exception;
+
 }
