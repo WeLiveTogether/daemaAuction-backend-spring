f8923dcfc9d780af3db0961cb26a394e55baae58
SonYoonSeok <dbstjr5517@naver.com>
2021-11-21 23:14:47 +0900

    :bug: :: ProductStatis 삭제

--- src/main/java/mungsanbackend/daemaAuction/web/dto/request/ProductRequest.java
+++ src/main/java/mungsanbackend/daemaAuction/web/dto/request/ProductRequest.java
@@ -2,7 +2,6 @@ package mungsanbackend.daemaAuction.web.dto.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
-import mungsanbackend.daemaAuction.domain.ProductSaleStatus;
 
 @Getter
 public class ProductRequest {
@@ -17,9 +16,6 @@ public class ProductRequest {
 
     @Schema(description = "경매시작가", example = "1000")
     private int auctionPrice;
-
-    // 물품 판매 상태 enum
-    private ProductSaleStatus saleStatus;
 
     @Schema(description = "메인카테고리 이름", example = "도서")
     private String category;
